6c1a30d0e4703b59641107300bf1591709e99609
TheLudway <theludway@gmail.com>
2025-04-27 14:43:52 -0500

    Datos de descarga filtrados

--- src/CleanData.py
+++ src/CleanData.py
@@ -18,21 +18,41 @@ def _():
 
 
 @app.cell
+def _(mo):
+    mo.md(
+        r"""
+        # Limpieza de Datos
+
+        Después de hacer web scrapping, los datos de las fechas quedan muy desordenados.
+        """
+    )
+    return
+
+
+@app.cell
 def _(pd):
     df = pd.DataFrame(pd.read_csv("../Data/Levels/niveles.csv"))
     return (df,)
 
 
 @app.cell
-def _(df):
+def _(df, pd):
     df_no_duplicates = df.drop_duplicates(subset=["Date"], ignore_index=True)
-    df_no_duplicates
+    df_no_duplicates.rename(columns={"Value":"Value (m)"}, inplace=True)
+    df_no_duplicates["Date"] = pd.to_datetime(df_no_duplicates["Date"], format="%b/%d/%y")
+    df_no_duplicates.sort_values(by="Date", ascending=True)
     return
 
 
 @app.cell
 def _():
     return
+
+
+@app.cell
+def _():
+    import marimo as mo
+    return (mo,)
 
 
 if __name__ == "__main__":
