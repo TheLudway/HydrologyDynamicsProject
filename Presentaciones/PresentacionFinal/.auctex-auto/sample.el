d16ed02b83f541fd7f40893bb0ed378af464a61d
TheLudway <theludway@gmail.com>
2025-05-14 13:27:57 -0500

    Presentación Final

--- Presentaciones/PresentacionFinal/.auctex-auto/sample.el
+++ Presentaciones/PresentacionFinal/.auctex-auto/sample.el
@@ -6,18 +6,18 @@
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("beamer" "17pt" "t" "lualatex")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
-                     '(("amsmath" "") ("amssymb" "") ("mathtools" "") ("babel" "spanish") ("biblatex" "") ("hyperref" "") ("xurl" "") ("svg" "") ("listings" "") ("ccicons" "scale=7")))
-   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
+                     '(("amsmath" "") ("amssymb" "") ("mathtools" "") ("graphicx" "") ("babel" "spanish") ("biblatex" "") ("hyperref" "") ("xurl" "") ("svg" "") ("listings" "") ("ccicons" "scale=7")))
    (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
    (TeX-run-style-hooks
     "latex2e"
     "beamer"
@@ -25,6 +25,7 @@
     "amsmath"
     "amssymb"
     "mathtools"
+    "graphicx"
     "babel"
     "biblatex"
     "hyperref"
